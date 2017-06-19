f7f352907ec8ea782799caf07ce50ba24609e288
Victor Allen <vamuigua@gmail.com>
2017-06-19 19:01:40 +0300

    Modified arrangment of attributes

--- app/models/post.js
+++ app/models/post.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   author: DS.attr('string'),
+  text: DS.attr('string'),
   createdDate: DS.attr('date'),
-  text: DS.attr('string')
 });
